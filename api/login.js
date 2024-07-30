1ebb4bf6ccfe5d9a4867ecd14d976706b38f5d8d
Salman1205 <muhammad.salman1205@gmail.com>
2024-07-30 15:28:52 +0500

    added console log

--- api/login.js
+++ api/login.js
@@ -18,6 +18,9 @@ connection.connect(err => {
 });
 
 export default async function handler(req, res) {
+
+    console.log("function called");
+
     // CORS setup
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
